b61cab9e5e08725f9ff7f7df3a4105bf035efe39
feaxxae <Duncan.Westerdijk@quicknet.nl>
2021-03-04 12:51:04 +0100

    Feat: Save/Load is now possible (still requires final testing) Unable to fully test save load system for now due to not having values to save. Additionally created StaticManager.cs, more information about the class in the summary within.

---
+++ Assets/Scripts/DataManagement/StaticManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script is to manage temporary values while the gameloop is running.
+/// every value set here will be cleared after closing the game.
+/// </summary>
+public static class StaticManager
+{
+    //Example value
+    public static int exampleInt;
+    public static int exampleInt2;
+    public static int exampleInt3;
+}
