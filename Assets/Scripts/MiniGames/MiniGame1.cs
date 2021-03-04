467eb1a33a59ec5120be0bcf994249594f05d8e8
blizzard190 <22256@ma-web.nl>
2021-03-04 14:45:57 +0100

    Feature: Made small AR minigame untested

--- Assets/Scripts/MiniGames/MiniGame1.cs
+++ Assets/Scripts/MiniGames/MiniGame1.cs
@@ -16,9 +16,41 @@ public class MiniGame1 : MonoBehaviour
         Instantiate(_target, sPos, Quaternion.identity);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+#if UNITY_ANDROID
+        for (var i = 0; i < Input.touchCount; ++i)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+                RaycastHit hit;
+                // Construct a ray from the current touch coordinates
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+                // Send message if hit
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Transform objectHit = hit.transform;
+
+                    objectHit.SendMessage("GotHit", SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        }
+#endif
+
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Transform objectHit = hit.transform;
+
+                Debug.Log(objectHit.gameObject.name);
+                objectHit.gameObject.SendMessage("GotHit", SendMessageOptions.DontRequireReceiver);
+            }
+        }
+#endif
     }
 }
---
+++ Assets/Scripts/MiniGames/TargetBehavior.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBehavior : MonoBehaviour
+{
+    public void GotHit()
+    {
+        //Set Score.
+        Destroy(this.gameObject);
+    }
+}
