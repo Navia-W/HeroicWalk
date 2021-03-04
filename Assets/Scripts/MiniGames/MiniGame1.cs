e58052c6eca3fe848e29cc55774931395c6d95aa
blizzard190 <22256@ma-web.nl>
2021-03-04 13:56:27 +0100

    Feature: Added minigame manager and minigame 1 set up

---
+++ Assets/Scripts/MiniGames/MiniGame1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGame1 : MonoBehaviour
+{
+    public float range = 1;
+
+    [SerializeField]
+    private GameObject _target;
+
+    void Start()
+    {
+        Vector2 rPos = Random.insideUnitCircle * range;
+        Vector3 sPos = new Vector3(rPos.x, 0, rPos.y);
+        Instantiate(_target, sPos, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MiniGames/MiniGameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGameManager : MonoBehaviour
+{
+    public static MiniGameManager miniGameManager;
+
+    [SerializeField]
+    private List<GameObject> _miniGames;
+    private GameObject _activeMiniGame;
+
+    private void Awake()
+    {
+        SelectMiniGame();
+    }
+
+    private void SelectMiniGame()
+    {
+        _activeMiniGame = Instantiate(_miniGames[Random.Range(0, _miniGames.Count)]);
+    }
+
+    
+    public void OnComplete()
+    {
+        //Save score
+        //Load Main scene
+    }
+}
